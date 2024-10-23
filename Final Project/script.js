e3dee974ab1152c4b85d2142b83cb9fc45b9ddf6
florentsahiti <66284617+florentsahiti@users.noreply.github.com>
2024-10-23 17:32:20 +0200

    displaying a map marker

--- Final Project/script.js
+++ Final Project/script.js
@@ -1,12 +1,55 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      //console.log(position)
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      //linkun qe eshte brenda template literal duhet me e marr ne google maps ne shembullin ne vijim https://www.google.com/maps/@42.464118,21.468203
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map("map").setView(coords, 13);
+
+      L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      //map eshte nje object i gjenereuar nga leaflet
+      map.on("click", function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+
+        //Instantiates a Marker object given a geographical point and optionally an options object.
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+
+          }))
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+    },
+    function () {
+      alert("Could not get your position");
+    }
+  );
